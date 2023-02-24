db53e76764598a076c7ed71de6c7a0ecdca0ef08
Timur Lagawy <timurtagirovich@mail.ru>
2023-02-24 16:15:35 +0100

    calculator_day_2_addButtons

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,80 +4,52 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
      public class Calculator extends JFrame {
-        JTextField name_field;
+        JTextField num_field;
         JTextField email_field;
         JRadioButton male, female;
         JCheckBox check;
         public Calculator(){
             super("Calculator");
-            super.setBounds(200, 100, 300, 230);
+            super.setBounds(200, 100, 400, 400);
             super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
             Container container = super.getContentPane();
-            container.setLayout(new GridLayout( 5, 2, 2,10));
-            JLabel name = new JLabel("Input your name ");
-            name_field = new JTextField("", 1);
-            JLabel email = new JLabel("Input your e-mail ");
-            email_field = new JTextField("@.", 1);
-
-            container.add(name);
-            container.add(name_field);
-            container.add(email);
-            container.add(email_field);
-
-           // male = new JRadioButton("Man");
-            //female = new JRadioButton("Woman");
-            //check = new JCheckBox("I am sure ", false);//если ТРУ - то галочка стоит
+            container.setLayout(new GridLayout( 5, 4, 2,10));
+            JLabel num = new JLabel("Input your numerous ");
+            num_field = new JTextField("", 1);
+            container.add(num);
+            container.add(num_field);
             JButton one_button = new JButton("1");
-            JButton two_button = new JButton("2");
-            JButton three_button = new JButton("3");
-            JButton four_button = new JButton("4");
-            JButton five_button = new JButton("5");
-            JButton six_button = new JButton("6");
-            JButton seven_button = new JButton("7");
-            JButton eight_button = new JButton("8");
-            JButton nine_button = new JButton("9");
-            JButton zero_button = new JButton("0");
-
-
-//            male.setSelected(true);//изначальный выбор в галочкой
-//            container.add(male);
-//            container.add(female);
-//            ButtonGroup group = new ButtonGroup();
-//            group.add(male);
-//            group.add(female);
-
-            container.add(check);
             container.add(one_button);
+            JButton two_button = new JButton("2");
             container.add(two_button);
+            JButton three_button = new JButton("3");
             container.add(three_button);
+            JButton four_button = new JButton("4");
             container.add(four_button);
+            JButton five_button = new JButton("5");
             container.add(five_button);
+            JButton six_button = new JButton("6");
             container.add(six_button);
+            JButton seven_button = new JButton("7");
             container.add(seven_button);
+            JButton eight_button = new JButton("8");
             container.add(eight_button);
+            JButton nine_button = new JButton("9");
             container.add(nine_button);
+            JButton zero_button = new JButton("0");
             container.add(zero_button);
 
+            ButtonGroup group = new ButtonGroup();
+            group.add(one_button);
+           group.add(two_button);
+           group.add(three_button);
+           group.add(four_button);
+           group.add(five_button);
 
-          //  send_button.addActionListener(new ButtonEventManager());
-        }
-       // class ButtonEventManager implements ActionListener {
 
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                String name = name_field.getText();
-//                String email = email_field.getText();
-//
-//                String isMale = "man";
-//                if(!male.isSelected())
-//                    isMale="Female";
-//
-//                boolean checkBox = check.isSelected();
-//                JOptionPane.showMessageDialog(null, "Your mail: " + email +
-//                                "\n Your male:  " + isMale + "\n Do you accept? " + checkBox,
-//                        "Hello " + name, JOptionPane.PLAIN_MESSAGE);
-            }
+
+            }}
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
         public static void main (String[]args){
             Calculator calculator = new Calculator();
             calculator.setVisible(true);
+
         }
     }
 
